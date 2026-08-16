9a53809e68def1aab05622255ed4fc4bb6c8595f
rcdevstudios <rcdevstudios@gmail.com>
2026-08-16 21:38:12 +0530

    Deploy setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
+
   migrations: {
-    path: './prisma/migrations',
+    path: "prisma/migrations",
   },
+
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
   },
 });
+  

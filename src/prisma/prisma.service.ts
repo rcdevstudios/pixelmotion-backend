c0d53c19cc3e4e8517a8494c098a86281c8d6736
rcdevstudios <rcdevstudios@gmail.com>
2026-08-16 21:24:02 +0530

    Deploy setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,20 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const connectionString = process.env.DATABASE_URL;
-
-    if (!connectionString) {
-      throw new Error(
-        'DATABASE_URL is missing. Add it to your .env file or environment variables.',
-      );
-    }
-
-    const adapter = new PrismaPg({ connectionString });
-    super({ adapter });
+    super();
   }
 
   async onModuleInit() {

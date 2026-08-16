e73f7fb63d70d10d7aa0738c8f8072dc90d105e1
rcdevstudios <rcdevstudios@gmail.com>
2026-08-16 22:55:00 +0530

    DB URL Updated

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-    });
-    super({ adapter });
+    super();
   }
 
   async onModuleInit() {

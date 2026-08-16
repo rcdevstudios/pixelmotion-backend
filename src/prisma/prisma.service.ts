e93faa0477fca632ff093ef5ee139b7793debb46
rcdevstudios <rcdevstudios@gmail.com>
2026-08-16 23:04:28 +0530

    Direct URL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import * as dns from 'dns';
-
-// Force Node.js to resolve IPv4 addresses first.
-// This works around Render's lack of outbound IPv6 support
-// when connecting to Supabase which has both IPv4 and IPv6.
-dns.setDefaultResultOrder('ipv4first');
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
